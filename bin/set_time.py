663105f0109b4ef008e9b3a0a59f5514821385b1
Aravind Sundaresan <asundaresan@gmail.com>
2016-11-03 21:40:09 -0700

    Add set datetime to set_time.py

--- bin/set_time.py
+++ bin/set_time.py
@@ -5,13 +5,23 @@ from datetime import datetime, timedelta
 from gi.repository import GExiv2
 
 
-def set_date_offset( filename, delta, verbose = 0 ):
+def get_datetime( filename ):
   exif = GExiv2.Metadata( filename )
-  date = exif.get_date_time()
+  return exif.get_date_time()
+
+
+def set_datetime( filename, dt ):
+  exif = GExiv2.Metadata( filename )
+  exif.set_date_time( dt )
+  exif.save_file()
+
+
+def set_date_offset( filename, delta, verbose = 0 ):
+  date = get_datetime( filename )
   date2 = date + delta
   print( "Setting datetime for %s: %s to %s" % ( filename, date, date2 ) )
-  exif.set_date_time( date2 )
-  exif.save_file()
+  set_datetime( filename, dt )
+
 
 def get_timedelta( value ):
   delta = timedelta()
@@ -23,6 +33,7 @@ def get_timedelta( value ):
       delta = delta + timedelta( hours = int( v.rstrip( "h" ) ) )
   return delta
 
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument( "images", nargs = "+", help="Images to modify" )
@@ -31,12 +42,22 @@ if __name__ == "__main__":
     help="Increment timestamp as a CSV (E.g. 5d,7h is 5 days and 7 hours)" )
   parser.add_argument( "--decrement", "-d", type = str, default = "",
     help="Decrement timestamp as a CSV " )
+  parser.add_argument( "--datetime", "-s", type = str, default = "",
+    help="Set timestamp e.g. \"2014-06-03 8:21\"" )
   args = parser.parse_args()
 
   if args.increment != None or args.decrement != None:
     delta = get_timedelta( args.increment ) - get_timedelta( args.decrement )
     for f in args.images:
       set_date_offset( f, delta )
-
+  elif args.datetime != "":
+    try: 
+      dt = datetime.strptime( arg.datetime, "%Y-%m-%d %H:%M" )
+    except:
+      print( "Failed to parse datetime string: %s" % args.datetime )
+      print( "  E.g.: 2014-06-03 21:33" )
+    finally:
+      for f in args.images:
+        set_datetime( f, dt )
 
 
